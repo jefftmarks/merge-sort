f7ce6a72fdd02f8d973e5d4138e3fd1347089158
Jeff Marks <jeff.t.marks@gmail.com>
2022-10-09 21:19:17 -0400

    done with assignment

--- javascript/merge_sort.js
+++ javascript/merge_sort.js
@@ -1,13 +1,37 @@
 function merge(arr1, arr2) {
-  // type your code here
+  const output = [];
+
+	while (arr1.length > 0 && arr2.length > 0) {
+		if (arr1[0] <= arr2[0]) {
+			output.push(arr1.shift());
+		} else {
+			output.push(arr2.shift());
+		}
+	}
+
+	if (arr1.length === 0) {
+		return output.concat(arr2);
+	} else {
+		return output.concat(arr1);
+	}
 }
 
 function mergeSort(arr) {
-  // type your code here
+  if (arr.length < 2) {
+		return arr;
+	}
+
+	const middle = Math.floor(arr.length / 2);
+	const left = arr.slice(0, middle);
+	const right = arr.slice(middle);
+
+	return merge(mergeSort(left), mergeSort(right));
 }
 
 if (require.main === module) {
   // add your own tests in here
+	// console.log(merge([38], [27]));
+
   console.log("Expecting: [1, 2]");
   console.log("=>", mergeSort([2, 1]));
 
